7aaae2d0ed07cae86aff2c12596c499a5d870104
SachinKinha <112786050+nk-code-lab@users.noreply.github.com>
2023-05-05 09:53:09 +0530

    back process by using request method

--- main.py
+++ main.py
@@ -1,62 +1,87 @@
+import requests
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 import chromedriver_autoinstaller
-from bs4 import BeautifulSoup
 import time
 import sys
 import os
 
-CHROME_WHATSAPP_PROFILE = "user-data-dir=C:\\Users\\nikhi\\AppData\\Local\\Google\\Chrome\\User Data\\whatsapp"
-chromedriver_autoinstaller.install()
-option = webdriver.ChromeOptions()
-option.add_argument(CHROME_WHATSAPP_PROFILE)
-driver = webdriver.Chrome(options=option)
-
 RESULT = True
 
-IGNOU_URL = "https://iop.ignouonline.ac.in/announcements/0"
-driver.get(IGNOU_URL)
+url = 'https://iop.ignouonline.ac.in/announcements/0'
 
+R = requests.get(url)
+htmlContent = R.content
+# print(htmlContent)
+
+soup = BeautifulSoup(htmlContent, 'html.parser')
+# print(soup)
+
+# -------------------------------------------
+        # Checking for new data
+# -------------------------------------------
+table = soup.find('table')
+# print(table.get_text())
+tableBody = table.find('tbody')
+newNotification = tableBody.find('tr')
 
 NEW_NOTICE_INDEX = 0
 with open('counter.txt','r', encoding='utf8') as f:
     NEW_NOTICE_INDEX = f.read()
     NEW_NOTICE_INDEX = int(NEW_NOTICE_INDEX)
+# print(NEW_NOTICE_INDEX)
 
-EYE1 = f"preview{NEW_NOTICE_INDEX}"
+notice = f"notice{NEW_NOTICE_INDEX}"
+NOTICE_INDEX = newNotification.find('div', id= notice)
 
+# print(NOTICE_INDEX)
 
-try:
-    NOTICE_EYE = WebDriverWait(driver, 5).until(EC.presence_of_element_located((By.ID, EYE1))).click()
-    os.system('cls')
-except Exception as e:
+if NOTICE_INDEX == None:
     RESULT = False
-    Message = f"Sorry! no new notice yet{Keys.SHIFT}{Keys.ENTER}{Keys.SHIFT}{Keys.ENTER}"
+    print('Sorry!!')
+    os.system('exit')
+else:
+    RESULT = True
 
-driver.close()
-
+# -------------------------------------------
+        # Checking end
+# -------------------------------------------
 if RESULT:
-    Message = f'''Dear friends
-    One new notice recived
-    Please check the website : https://iop.ignouonline.ac.in/announcements/0 
-    Thanks and Regards
-    Admin Bot
-    {Keys.SHIFT}{Keys.ENTER}   
-    {Keys.SHIFT}{Keys.ENTER}
+    notification = newNotification.get_text()
+    # print(newNotification.get_text().strip())
+    link = newNotification.find('a')
+    newLink = link['href']
 
-    {Keys.SHIFT}{Keys.ENTER}
-    
+    Message = f'''
+        Hey Everyone,
+
+        {notification}
+
+        Click here : {newLink}
+
+        Thanks and Regards
+        Admin Bot
+        \n
+
+
     '''
-
+    # print(Message)
+if RESULT:
+# -----------------------------------------------
+#       Increasing index by + 1
+# -----------------------------------------------
     with open('counter.txt','w', encoding='utf8') as f:
         NEW_NOTICE_INDEX += 1
         f.write('{}' .format(NEW_NOTICE_INDEX))
 
+# -----------------------------------------------
+#       Sending message to WhatsApp Community
+# -----------------------------------------------
 
-if RESULT:
     CHROME_WHATSAPP_PROFILE = "user-data-dir=C:\\Users\\nikhi\\AppData\\Local\\Google\\Chrome\\User Data\\whatsapp"
     chromedriver_autoinstaller.install()
     option = webdriver.ChromeOptions()
@@ -64,18 +89,19 @@ if RESULT:
     driver = webdriver.Chrome(options=option)
     url = "https://web.whatsapp.com/"
     driver.get(url)
-    # time.sleep(1)
+    time.sleep(5)
 
     search_text = "IGNOU STUDENTS COMMUNITY"
     search_path = '//div[@contenteditable="true"][@data-tab="3"]'
     SEARCH = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, search_path)))
     SEARCH.send_keys(search_text)
-    # time.sleep(1)
+    time.sleep(5)
     xpath = f'//span[@title="{search_text}"]'
     OPEN = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath))).click()
     input_path = '//div[@contenteditable="true"][@data-tab="10"]'
     INPUT_MSG = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, input_path)))
     INPUT_MSG.send_keys(Message)
-    # time.sleep(1)
+    time.sleep(5)
+    driver.quit()
 
-driver.quit()
+os.system('exit')
